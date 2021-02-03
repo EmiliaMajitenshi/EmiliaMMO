134a0f219a504e710497addec9380f3ee0c18fea
EmiliaMajiTenshi <kenneth_tl@outlook.com>
2021-02-03 15:25:47 -0800

    Skill Order update

--- src/main/java/me/EmiliaMMO/listeners/PlayerListeners.java
+++ src/main/java/me/EmiliaMMO/listeners/PlayerListeners.java
@@ -1,21 +1,22 @@
 package me.EmiliaMMO.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import me.EmiliaMMO.skill.SkillOrder;
 
 public class PlayerListeners implements Listener {
     public Player p;
-    public String[] skillOrder = { "-", "-", "-" };
-    public int counter = 0;
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerUse(PlayerInteractEvent event) {
 
         boolean playerLeftClick = event.getAction() == Action.LEFT_CLICK_AIR;
@@ -25,45 +26,47 @@ public class PlayerListeners implements Listener {
         p = (Player) event.getPlayer();
 
         if (playerLeftClick && (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SHOVEL)) {
-            if (counter != 2) {
-                skillOrder[counter] = "Left";
-                counter = counter + 1;
-                SkillOrder.skillOrderReminder(player, skillOrder);
-            } else if (counter == 2) {
-                skillOrder[counter] = "Left";
-                SkillOrder.skillReleasedReminder(player, skillOrder);
-                if (skillOrder[0].equals("Left") && skillOrder[1].equals("Left") && skillOrder[2].equals("Left")) {
+            if (!SkillOrder.isSkillReady(player)) {
+                SkillOrder.setLatestSkillOrder(player, "Left");
+                SkillOrder.skillOrderReminder(player, SkillOrder.getMapPlayerSkillOrder(player));
+            } else if (SkillOrder.isSkillReady(player)) {
+                SkillOrder.setLatestSkillOrder(player, "Left");
+                SkillOrder.skillOrderReminder(player, SkillOrder.getMapPlayerSkillOrder(player));
+                if (SkillOrder.getMapPlayerSkillOrder(player)[0].equals("Left")
+                        && SkillOrder.getMapPlayerSkillOrder(player)[1].equals("Left")
+                        && SkillOrder.getMapPlayerSkillOrder(player)[2].equals("Left")) {
                     player.getLocation().getWorld().createExplosion(player.getTargetBlock(null, 12).getLocation(), 2,
                             false, false);
-                } else if (skillOrder[0].equals("Left") && skillOrder[1].equals("Right")
-                        && skillOrder[2].equals("Left")) {
+                } else if (SkillOrder.getMapPlayerSkillOrder(player)[0].equals("Left")
+                        && SkillOrder.getMapPlayerSkillOrder(player)[1].equals("Right")
+                        && SkillOrder.getMapPlayerSkillOrder(player)[2].equals("Left")) {
                     player.getWorld()
                             .spawnEntity(player.getEyeLocation().add(player.getLocation().getDirection()),
                                     EntityType.SNOWBALL)
                             .setVelocity(player.getEyeLocation().add(player.getLocation().getDirection()).getDirection()
                                     .multiply(4.0));
                 }
-                counter = 0;
-                for (int i = 0; i < skillOrder.length; i++) {
-                    skillOrder[i] = "-";
-                }
+                SkillOrder.initMap(player);
             }
 
         } else if (playerRightClick
                 && (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SHOVEL)) {
-            if ((counter != 2) && (counter != 0)) {
-                skillOrder[counter] = "Right";
-                counter = counter + 1;
-                SkillOrder.skillOrderReminder(player, skillOrder);
-            } else if (counter == 2) {
-                skillOrder[counter] = "Right";
-                SkillOrder.skillReleasedReminder(player, skillOrder);
-                counter = 0;
-                for (int i = 0; i < skillOrder.length; i++) {
-                    skillOrder[i] = "-";
-                }
+            if ((!SkillOrder.isSkillReady(player)) && (SkillOrder.noAutoAttackStart(player))) {
+                SkillOrder.setLatestSkillOrder(player, "Right");
+                SkillOrder.skillOrderReminder(player, SkillOrder.getMapPlayerSkillOrder(player));
+            } else if (SkillOrder.isSkillReady(player)) {
+                SkillOrder.setLatestSkillOrder(player, "Right");
+                SkillOrder.skillOrderReminder(player, SkillOrder.getMapPlayerSkillOrder(player));
+                SkillOrder.initMap(player);
             }
 
         }
     }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        e.setJoinMessage(ChatColor.GOLD + "Hrac " + e.getPlayer().getName() + " joined");
+        SkillOrder.initMap(e.getPlayer());
+    }
+
 }
--- src/main/java/me/EmiliaMMO/skill/SkillOrder.java
+++ src/main/java/me/EmiliaMMO/skill/SkillOrder.java
@@ -2,11 +2,52 @@ package me.EmiliaMMO.skill;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.Sound;
 
 public class SkillOrder {
+    static HashMap<UUID, String[]> skillOrderMap = new HashMap<UUID, String[]>();
+
+    public static boolean isMapContainsPlayer(Player player) {
+        return skillOrderMap.containsKey(player.getUniqueId());
+    }
+
+    public static String[] getMapPlayerSkillOrder(Player player) {
+        return skillOrderMap.get(player.getUniqueId());
+    }
+
+    public static void initMap(Player player) {
+        skillOrderMap.put(player.getUniqueId(), new String[] { "-", "-", "-", "-", "-" });
+    }
+
+    public static boolean isSkillReady(Player player) {
+        if (getMapPlayerSkillOrder(player)[1].equals("-"))
+            return false;
+        return true;
+    }
+
+    public static boolean noAutoAttackStart(Player player) {
+        if (getMapPlayerSkillOrder(player)[0].equals("-"))
+            return false;
+        return true;
+    }
+
+    public static void setLatestSkillOrder(Player player, String key) {
+        String[] tempOrder = getMapPlayerSkillOrder(player);
+        for (int i = 0; i < tempOrder.length; i++) {
+            if (tempOrder[i].equals("-")) {
+                tempOrder[i] = key;
+                skillOrderMap.put(player.getUniqueId(), tempOrder);
+                return;
+            }
+        }
+    }
+
     public static void skillOrderReminder(Player player, String[] skillOrder) {
         String message = ChatColor.GOLD + skillOrder[0] + " " + skillOrder[1] + " " + skillOrder[2];
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
